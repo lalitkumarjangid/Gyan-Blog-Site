984f42c9c5b85b35bd987bda83c3c90ae6e54854
lalit <lkjlalitkumar2005@gmail.com>
2025-03-18 00:15:42 +0530

    Production

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,19 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['zwitch'], // Exclude zwitch from bundling
-    }
-  }
-});
+})
 
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     headers: {
+//       'Content-Security-Policy': "script-src 'self' 'unsafe-eval' 'unsafe-inline';"
+//     }
+//   }
+// });
